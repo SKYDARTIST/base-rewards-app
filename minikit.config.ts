277fa8aebfec8f93046b846f4ab0c929003d388e
cryptobulla <cryptobulla@cryptobullas-MacBook-Air.local>
2025-12-01 01:14:52 +0530

    added minikit config

---
+++ minikit.config.ts
@@ -0,0 +1,18 @@
+// Update this after Vercel deployment
+export const ROOT_URL = "https://base-rewards-app.vercel.app/";
+
+export const minikitConfig = {
+  accountAssociation: {
+    header: "",
+    payload: "",
+    signature: ""
+  },
+
+  miniapp: {
+    version: "1",
+    name: "Base Rewards Estimator",
+    subtitle: "Check your Base activity score instantly",
+    description: "A simple mini app that estimates your Base ecosystem rewards.",
+    
+    iconUrl: `${ROOT_URL}/icon.png`,
+    splashImageUrl: `${ROOT_URL}/spla_
